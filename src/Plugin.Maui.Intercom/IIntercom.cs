36ec3ad84b627a3426b9d38e30e46cba0358500e
Kori Francis <kfrancis@clinicalsupportsystems.com>
2025-05-15 10:47:58 -0400

    adjust the ios implementation

--- src/Plugin.Maui.Intercom/IIntercom.cs
+++ src/Plugin.Maui.Intercom/IIntercom.cs
@@ -1,18 +1,30 @@
 namespace Plugin.Maui.Intercom;
 
+/// <summary>
+///     Interface for Intercom plugin.
+/// </summary>
 public interface IIntercom
 {
     /// <summary>
-    /// Initialize Intercom with your API key and App ID.
+    ///     Initialize Intercom with your API key and App ID.
     /// </summary>
     /// <param name="apiKey">Your Intercom API key.</param>
     /// <param name="appId">Your Intercom App ID.</param>
     void Initialize(string apiKey, string appId);
 
+    /// <summary>
+    ///     Register an unidentified user.
+    /// </summary>
+    /// <param name="onSuccess">
+    ///     An optional callback used when the registration is successful.
+    /// </param>
+    /// <param name="onFailure">
+    ///     An optional callback used when the registration is not successful.
+    /// </param>
     void Register(Action? onSuccess = null, Action<string?>? onFailure = null);
 
     /// <summary>
-    /// Register a user using their userId 
+    ///     Register a user using their userId
     /// </summary>
     /// <param name="userId">The userId of the user you want to register</param>
     /// <param name="onSuccess">An optional callback used when the registration is successful</param>
@@ -21,7 +33,7 @@ public interface IIntercom
     void RegisterWithUserId(string userId, Action? onSuccess = null, Action<string?>? onFailure = null);
 
     /// <summary>
-    /// Register a user using their email 
+    ///     Register a user using their email
     /// </summary>
     /// <param name="email">The email address of the user you want to register</param>
     /// <param name="onSuccess">An optional callback used when the registration is successful</param>
@@ -29,19 +41,52 @@ public interface IIntercom
     /// <exception cref="ArgumentException">Thrown when the email is null or empty</exception>
     void RegisterWithEmail(string email, Action? onSuccess = null, Action<string?>? onFailure = null);
 
+    /// <summary>
+    ///     Log out the current user.
+    /// </summary>
     void Logout();
 
+    /// <summary>
+    ///     Secures the user session with Intercom.
+    /// </summary>
+    /// <param name="userHash">
+    ///     The HMAC hash of the userId and the app secret.
+    /// </param>
     void SetUserHash(string userHash);
 
+    /// <summary>
+    ///     Show the messenger with a predefined message.
+    /// </summary>
+    /// <param name="message">
+    ///     The message to be displayed in the messenger. If null, the messenger will be shown without a message.
+    /// </param>
     void PresentMessenger(string? message);
 
+    /// <summary>
+    ///     Show the help center to the user
+    /// </summary>
     void PresentHelpCenter();
 
+    /// <summary>
+    ///     Show the support center to the user
+    /// </summary>
     void PresentSupportCenter();
 
+    /// <summary>
+    ///     Show a carousel to the user.
+    /// </summary>
+    /// <param name="carouselId"></param>
     void PresentCarousel(string carouselId);
 
+    /// <summary>
+    ///     Show/Hide the messenger
+    /// </summary>
+    /// <param name="isVisible"></param>
     void SetVisible(bool isVisible);
 
+    /// <summary>
+    ///     Set the distance from the bottom of the screen to the messenger.
+    /// </summary>
+    /// <param name="bottomPadding"></param>
     void SetBottomPadding(int bottomPadding);
 }
--- src/Plugin.Maui.Intercom/Intercom.macios.cs
+++ src/Plugin.Maui.Intercom/Intercom.macios.cs
@@ -1,6 +1,8 @@
 using System;
+using Foundation;
 using MauiIntercomMaciOS;
 using IntercomSdk = MauiIntercomMaciOS.IntercomBinding;
+using IntercomSpace = MauiIntercomMaciOS.IntercomSpaceBinding;
 
 namespace Plugin.Maui.Intercom;
 
@@ -13,12 +15,32 @@ partial class IntercomImplementation : IIntercom
 
     public void Register(Action? onSuccess = null, Action<string?>? onFailure = null)
     {
-        throw new NotImplementedException();
+        IntercomSdk.LoginUnidentifiedUserWithCompletion((success, error) =>
+        {
+            if (success)
+            {
+                onSuccess?.Invoke();
+            }
+            else
+            {
+                onFailure?.Invoke(error?.LocalizedDescription);
+            }
+        });
     }
 
     public void RegisterWithUserId(string userId, Action? onSuccess = null, Action<string?>? onFailure = null)
     {
-        throw new NotImplementedException();
+        IntercomSdk.LoginUserWithUserId(userId, (success, error) =>
+        {
+            if (success)
+            {
+                onSuccess?.Invoke();
+            }
+            else
+            {
+                onFailure?.Invoke(error?.LocalizedDescription);
+            }
+        });
     }
 
     public void RegisterWithEmail(string email, Action? onSuccess = null, Action<string?>? onFailure = null)
@@ -38,32 +60,41 @@ partial class IntercomImplementation : IIntercom
 
     public void Logout()
     {
-        throw new NotImplementedException();
+        IntercomSdk.Logout();
     }
 
     public void SetUserHash(string userHash)
     {
-        // Do nothing
+        IntercomSdk.SetUserHash(userHash);
     }
 
-    public void PresentMessenger(string? message)
+    public void PresentMessenger(string? message = null)
     {
-        throw new NotImplementedException();
+        if (string.IsNullOrEmpty(message))
+        {
+            IntercomSdk.PresentIntercom();
+        }
+        else
+        {
+            IntercomSdk.PresentMessageComposer(message);
+        }
     }
 
     public void PresentHelpCenter()
     {
-        throw new NotImplementedException();
+        // Present the Help Center space
+        IntercomSdk.PresentIntercomWithSpace(IntercomSpace.HelpCenter);
     }
 
     public void PresentSupportCenter()
     {
-        throw new NotImplementedException();
+        // Present the Home space (which contains all Intercom functionality)
+        IntercomSdk.PresentIntercomWithSpace(IntercomSpace.Home);
     }
 
     public void PresentCarousel(string carouselId)
     {
-        throw new NotImplementedException();
+        IntercomSdk.PresentContentWith(carouselId, "carousel");
     }
 
     public void SetVisible(bool isVisible)
@@ -73,7 +104,86 @@ partial class IntercomImplementation : IIntercom
 
     public void SetBottomPadding(int bottomPadding)
     {
-        throw new NotImplementedException();
+        IntercomSdk.SetBottomPadding(bottomPadding);
     }
 
+    // Additional methods that might be useful for your implementation
+
+    public void LogEvent(string eventName)
+    {
+        IntercomSdk.LogEventWithName(eventName);
+    }
+
+    public void LogEvent(string eventName, Dictionary<string, object> metadata)
+    {
+        var nsDict = metadata.ToNSDictionary();
+        IntercomSdk.LogEventWithName(eventName, nsDict);
+    }
+
+    public void UpdateUser(string? userId = null, string? email = null, string? name = null,
+        string? phone = null, Dictionary<string, object>? customAttributes = null)
+    {
+        var userAttributes = IntercomSdk.CreateUserAttributes();
+
+        if (!string.IsNullOrEmpty(userId))
+            userAttributes.UserId = userId;
+
+        if (!string.IsNullOrEmpty(email))
+            userAttributes.Email = email;
+
+        if (!string.IsNullOrEmpty(name))
+            userAttributes.Name = name;
+
+        if (!string.IsNullOrEmpty(phone))
+            userAttributes.Phone = phone;
+
+        if (customAttributes != null)
+            userAttributes.CustomAttributes = customAttributes.ToNSDictionary();
+
+        IntercomSdk.UpdateUser(userAttributes, null);
+    }
+
+    public void HideMessenger()
+    {
+        IntercomSdk.HideIntercom();
+    }
+
+    public int GetUnreadConversationCount()
+    {
+        return (int)IntercomSdk.UnreadConversationCount();
+    }
+}
+
+// Extension method to convert Dictionary to NSDictionary
+public static class DictionaryExtensions
+{
+    public static NSDictionary<NSString, NSObject> ToNSDictionary(this Dictionary<string, object> dictionary)
+    {
+        var nsDict = new NSDictionary<NSString, NSObject>();
+
+        foreach (var item in dictionary)
+        {
+            NSObject nsValue;
+
+            // Convert C# types to NSObject types
+            if (item.Value is string strValue)
+                nsValue = new NSString(strValue);
+            else if (item.Value is int intValue)
+                nsValue = new NSNumber(intValue);
+            else if (item.Value is double doubleValue)
+                nsValue = new NSNumber(doubleValue);
+            else if (item.Value is bool boolValue)
+                nsValue = new NSNumber(boolValue);
+            else if (item.Value is DateTime dateValue)
+                nsValue = (NSDate)dateValue;
+            else if (item.Value is Dictionary<string, object> dictValue)
+                nsValue = dictValue.ToNSDictionary();
+            else
+                nsValue = NSObject.FromObject(item.Value);
+
+            nsDict.SetValueForKey(nsValue, new NSString(item.Key));
+        }
+
+        return nsDict;
+    }
 }
